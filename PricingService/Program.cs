aad4fa8dd1a61fde844d659f965c93acdeeeaf51
Fernando Matías Cruz <matias18233@gmail.com>
2021-01-16 18:17:44 -0300

    Se agrega ejercicio TTD con refactorización

---
+++ PricingService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PricingService {
+    public class Program {
+        static void Main(string[] args) {
+
+        }
+        public float CalcularPrecioTotal() {
+            float total = 0;
+            return total;
+        }
+    }
+}
---
+++ PricingServiceTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PricingService;
+
+namespace PricingServiceTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            Program servicioPrecio = new Program();
+
+        }
+    }
+}
