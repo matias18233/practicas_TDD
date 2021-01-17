b78659f3e6c106a03f69f365184930d8c8cb00c9
Fernando Matías Cruz <matias18233@gmail.com>
2021-01-17 13:35:07 -0300

    Aplicando más refactorización

--- PricingService/Program.cs
+++ PricingService/Program.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 namespace PricingService {
     public class Program {
-        float[] _orden;
+        private float[] _orden;
         public float[] Orden {
             get => _orden;
             set => _orden = value;
         }
-        valoresExtra valoresExtra;
+        private valoresExtra valoresExtra;
         public valoresExtra ValoresExtra {
             get => valoresExtra;
             set => valoresExtra = value;
+        }
+        private float precioOrden;
+        public float PrecioOrden {
+            get => precioOrden;
+            set => precioOrden = value;
         }
         static void Main(string[] args) {
 
@@ -22,20 +27,23 @@ namespace PricingService {
         public Program(float[] Porden, valoresExtra PvaloresExtra) {
             Orden = Porden;
             ValoresExtra = PvaloresExtra;
+            PrecioOrden = Porden[0];
         }
         public float CalcularPrecioTotal() {
-            float precioOrden = aplicarDescuento() + aplicarCargosEnvio();
-            float total = aplicarImpuestos(precioOrden);
-            return total;
+            aplicarDescuento();
+            aplicarCargosEnvio();
+            aplicarImpuestos();
+            
+            return PrecioOrden;
         }
-        private float aplicarDescuento() {
-            return Orden[0] - Orden[1];
+        private void aplicarDescuento() {
+            PrecioOrden -= Orden[1];
         }
-        private float aplicarCargosEnvio() {
-            return ValoresExtra.GastoEnvio;
+        private void aplicarCargosEnvio() {
+            PrecioOrden += ValoresExtra.GastoEnvio;
         }
-        private float aplicarImpuestos(float PprecioOrden) {
-            return PprecioOrden * ValoresExtra.Impuesto;
+        private void aplicarImpuestos() {
+            PrecioOrden *= ValoresExtra.Impuesto;
         }
     }
 }
