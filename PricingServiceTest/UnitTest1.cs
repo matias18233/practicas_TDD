ae2c2a9c6f49126a63456cd54b36a3138c481b98
Fernando Matías Cruz <matias18233@gmail.com>
2021-01-17 13:35:17 -0300

    Agregado de nueva prueba

--- PricingServiceTest/UnitTest1.cs
+++ PricingServiceTest/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace PricingServiceTest {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void TestMethod1() {
+        public void precioConDescuento() {
             float[] valores = new float[2];
             valores[0] = 100;
             valores[1] = 10;
@@ -18,6 +18,20 @@ namespace PricingServiceTest {
             float precioTotal = servicioPrecio.CalcularPrecioTotal();
             
             Assert.AreEqual(precioTotal, 161.00);
+        }
+        [TestMethod]
+        public void precioSinDescuento() {
+            float[] valores = new float[2];
+            valores[0] = 100;
+            valores[1] = 0;
+
+            valoresExtra valoresExtra = new valoresExtra(15, 50);
+
+            Program servicioPrecio = new Program(valores, valoresExtra);
+
+            float precioTotal = servicioPrecio.CalcularPrecioTotal();
+
+            Assert.AreEqual(precioTotal, 172.50);
         }
     }
 }
